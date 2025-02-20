869e8bdfc81d420817bc13dcdd4a3853bde5d699
Kevin Pereda <kptwdo15@gmail.com>
2025-02-19 23:11:37 -0500

    Everything that was modified including the package.json files is just a result of me adding tailwind packages to the project. Tailwind is now up and runnning for any components made. Just use className and have fun inline styling. Imported tailwind to index.css since it is referenced in main.tsx the entry point for the app. Also changed the vite.config file to let it know tailwind is being used.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' //added tailwind functionality to vite project
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
