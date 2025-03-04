9752de8c3d4460f2101a7659095a5860fdfda037
Daniel <danielperera0111@gmail.com>
2025-03-04 16:42:07 -0500

    Changed the login page file to include a button that redirects to the register page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,15 +1,21 @@
-import PageTitle from '../components/PageTitle.tsx';
-import Login from '../components/Login.tsx';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageTitle from '../components/PageTitle';
+import Login from '../components/Login';
 
-const LoginPage = () =>
-{
-
-    return(
-      <div>
-        <PageTitle />
-        <Login />
-      </div>
-    );
+const LoginPage: React.FC = () => {
+  return (
+    <div>
+      <PageTitle />
+      <Login />
+      <p>
+        Don't have an account?{' '}
+        <Link to="/register" style={{ color: 'blue', textDecoration: 'underline' }}>
+          Register here
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default LoginPage;
