ddd0669afac9c56cf307c9e7633ce0e5a48b11c8
Kevin Pereda <kptwdo15@gmail.com>
2025-03-05 22:33:00 -0500

    Finished the landing page. All I need to do is add the text and interactivty with login and about buttons. By interactivity I mean onClick route them to other pages.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,18 +1,42 @@
+import plantImage from "../assets/LandingPageImages/cartoonPlant.png";
+import closetImage from "../assets/LandingPageImages/closet.png";
+import blackheart from "../assets/LandingPageImages/blackheart.png";
+import chatbubble from "../assets/LandingPageImages/chat_bubble.png";
+import sendbutton from "../assets/LandingPageImages/send.png";
+import bookmark from "../assets/LandingPageImages/bookmark.png";
+import logo from "../assets/GreenLogo.png";
 
 export default function LandingPage(){
-  console.log("hello");
   return (
 
-    <div >
+    <div className = "relative flex justify-start items-start h-screen pt-24 gap-20 -translate-x-36"> {/* Outer div takes up whole screen so the inner div can be centered on screen */}
 
-      <h1>hello</h1>
+          <div className = "relative flex flex-col items-center justify-center bg-themeGray w-4/6 h-5/6 rounded-lg flex-shrink-0 gap-10"> {/*This div is the gray card containing the closet picture and buttons under it */}
 
+            <img src = {closetImage} alt = "Minimalist closet pic" className = "w-5/6 h-4/6 rounded-lg"></img> 
 
+            <div className = "flex flex-row justify-center items-center self-start gap-2 ml-16 w-fit"> {/*This div keeps all buttons in a flex row within the flex col that stacks the image and button above each other */}
+              <img src = {blackheart} alt = "heart symbol" className = "w-14 h-14"></img>
+              <img src = {chatbubble} alt = "chat bubble symbol" className = "w-14 h-14"></img>
+              <img src = {sendbutton} alt = "send button symbol" className = "w-14 h-14"></img>
+              <img src = {bookmark} alt = "bookmark symbol" className = " w-14 h-14 absolute right-16"></img>
+            </div>
+
+          </div>  
+
+          <div className="flex flex-col flex-shrink-0"> {/*Div for dressmeup logo ***FLEX COL CAUSE TEXT WILL GO UNDER IT IN THIS DIV */}
+            <img src = {logo} alt = "DressMeUp Logo" className = "w-44 h-44 rounded-lg" />
+          </div>
+
+          <div className = "flex flex-col items-center justify-center gap-10 flex-grow-0"> {/*Flex col so that login and about us are stacked on top of each other */}
+            <button className = "text-white text-3xl bg-themeGray border-4 border-themeDarkBeige w-44 h-16 rounded-lg"> Login  </button>
+            <button className = "text-white text-3xl bg-themeGray border-4 border-themeDarkBeige w-44 h-16 rounded-lg"> About Us </button>
+
+          </div>
+
+          <div className = "absolute -right-96 bottom-0"> {/*Plant picture is positioned abolsute (outside of page flow) relative to the screen div */}
+            <img src = {plantImage} alt = "plant pic" className = "w-96 h-80 " ></img>
+          </div>
     </div>
-
-
   );
-
-
-
 }
