c66eca58f3c7aa45af614f0439744342a2d9b412
Kevin Pereda <kptwdo15@gmail.com>
2025-03-08 18:09:29 -0500

    Added to main login page the dressmeupLogo on the top left as a button so if user wants to go back to landing page from the login page they can just click the logo.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
+import Logo from '../assets/GreenLogo.png';
 
 export default function LoginPage(){
   return (
-    <div>
-      <PageTitle />
-      <Login />
-      <p>
-        login page hehehe
-      </p>
+    <div className = "min-h-screen flex flex-row justify-center items-center relative">
+      
+      <Link to="/" className="-left-72 top-0 hover:scale-105 absolute"> {/*Using link here instead of a button cause we dont need any crazy functionaliity logo on top left of login screen will literally jusst take user back to landing page */}
+        <img src={Logo} alt="DressMeUp Logo" className = "w-44 h-44 rounded-lg" />
+      </Link>
+
+      <Login /> {/*Call login component which is login box with login and register form that changes depending on button user clicks */}
     </div>
   );
 };
