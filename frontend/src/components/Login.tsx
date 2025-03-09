dcff7515996a171ff5230d8f83ca192029dfa2c4
Kevin Pereda <kptwdo15@gmail.com>
2025-03-09 15:28:47 -0400

    Added to login.tsx the call to sliderButton component as part of the flex col. This component takes in the state of isLoginForm as well as the function that updates the state.

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,6 +1,7 @@
 // Updated Login.tsx
 import { useState } from 'react';
 import { buildPath as bp } from './Path.js';
+import SliderButton from './SliderButton.js';
 
 function Login() {
     //using the useState hook so that react can keepp track of these variables as  their state changes eg user types.
@@ -9,6 +10,8 @@ function Login() {
     const [loginPassword, setPassword] = useState('');
     //creating another useState variable that will keep track of what form to show user the login or register. Remeber state is dynamic in react if it changes react automatically re-renders page
     const [isLoginForm, setIsLoginForm] = useState(true); 
+
+    const toggleForm = () => setIsLoginForm((prev) => !prev); //declaring a callback function toggleForm that calls the setIsLoginForm which has a callback function inside thaat uses Functional Updating to flip the treu to false and vice versa. "prev" is what the state was before aand we flip it
 
     async function doLogin(event: any): Promise<void> {
         event.preventDefault();
@@ -46,8 +49,9 @@ function Login() {
     }
 
     return (
-        <div className = "flex flex-col bg-themeGray w-[600px] h-[600px] rounded-lg ">
-            
+        <div className = "flex flex-col justify-start items-center bg-themeGray w-[600px] h-[600px] rounded-lg ">
+
+            <SliderButton isLoginForm = {isLoginForm} toggleForm = {toggleForm}/> {/*Call slider button to display login / register button at top of div container. Passing it bool isLoginForm so we can keep track of state */}
             {/*In here we will have component for the login slider, the login form that will display when isLoginForm state is true, the register form that will
             display when false*/}
 
