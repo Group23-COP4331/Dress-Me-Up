6cba57ef28a2cb7367a3dda7742cf3f49ddc7be2
lain561 <mckissicethan@gmail.com>
2025-03-10 16:39:25 -0400

    Create ProfileCard.tsx

---
+++ frontend/src/components/ProfileCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export function profileCard(props){
+
+
+    return (
+        <div className='flex flex-col jusitfy-center items-center'>
+
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
