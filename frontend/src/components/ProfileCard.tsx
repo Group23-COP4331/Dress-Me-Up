d5331e620d8f42e15c976ae1e64954a6085c89c1
lain561 <mckissicethan@gmail.com>
2025-03-10 16:44:18 -0400

    Update ProfileCard.tsx

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-export function profileCard(props){
-
+export function profileCard(props){    
 
     return (
         <div className='flex flex-col jusitfy-center items-center'>
-
+            
         </div>
     );
 }
