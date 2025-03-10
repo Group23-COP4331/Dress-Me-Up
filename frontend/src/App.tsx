20542e9caf41099d8c3faa49c317e8007c58b817
Jesiel Reyes <jesiel.reyes2002@gmail.com>
2025-03-10 12:04:33 -0400

    added route for mycloset in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,14 @@ import './App.css';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import MyClosetPage from './pages/MyClosetPage';
 
 function App() {
   return (
     <Router> {/*This  wraps the entire app and makes it so we can use routing*/}
       <Routes> {/*This wraps all the routes and each <Route> component inside <Routes> defiines a path and a component that should be rendered when user visits that path*/}
         <Route path="/" element={<LandingPage />} />
+        <Route path ="/mycloset" element={<MyClosetPage/>}/>
         <Route path="*" element={<Navigate to="/" />} /> {/*  *path is a wildcard any unknown / undefined route will match that star and route user back to homepage */}
       </Routes>
     </Router>
