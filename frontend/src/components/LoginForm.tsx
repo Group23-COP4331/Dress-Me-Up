fda1a9cc90d6ad1ad0aa73f7cc07ebeab79fd94e
Kevin Pereda <kptwdo15@gmail.com>
2025-03-11 19:44:05 -0400

    Added the toggle password button functionality to the password input box. Used state to know when the user is toggling the button and updating the input to be of type password or type text. Hides and shows password.

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import { buildPath } from './BuildApiPath.js';
 
+//images
+import hide from '../assets/togglePassword/hide.png';
+import show from '../assets/togglePassword/show.png';
+
 interface LoginFormProps {
   setMessage: (message: string) => void; // Function that takes a string and returns nothing
 }
@@ -10,6 +14,7 @@ export default function LoginForm({setMessage} : LoginFormProps){
    //using the useState hook so that react can keepp track of these variables as  their state changes eg user types.
   const [emailName, setEmail] = useState('');
   const [loginPassword, setPassword] = useState('');
+  const [showPassword, setVisibility] = useState(false); //state so we can toggle password visbility. If show password is true we want to well show it. If its false then hide it
 
   //function gets called on submission of from async so we can use await
   async function doLogin(event: any): Promise<void> {
@@ -63,10 +68,14 @@ export default function LoginForm({setMessage} : LoginFormProps){
           <input onChange = {(e) => {setEmail(e.target.value)}} className = "w-[500px] h-12 rounded-lg pl-4" type="email" required id="email" name="email" placeholder='Youraddress@example.com' />
         </div>
 
-        <div className = "flex flex-col items-start gap-2">
+        <div className = "flex flex-col items-start gap-2 relative">
           <label htmlFor="password"> Password </label>
           {/*Make sure on input change we grab the event and call the callback funciton that sets the email state to whatever users key stroke was dyanmaic updating */}
-          <input onChange = {(e) => {setPassword(e.target.value)}} className = "w-[500px] h-12 rounded-lg pl-4" type="text" required id="password" name="password" placeholder='Password'/>
+          <input onChange = {(e) => {setPassword(e.target.value)}} className = "w-[500px] h-12 rounded-lg pl-4 " type= {showPassword? 'text': 'password'} required id="password" name="password" placeholder='Password'/>
+
+          <button className = {`absolute bottom-2 right-4 ${loginPassword ? '': 'opacity-50 cursor-not-allowed'}`} disabled={!loginPassword} onClick = {() => setVisibility((prev)=> !prev)}> {/*On click of this button we toggle the bool of our showPassword so that state changes and everything using show password re-renders. Also have disabled so that when password is not empty so !password it returns true which enables button to have onclick functionality */}
+            <img src = {showPassword? hide: show} alt = "toggle password icon" className = "w-8 h-8 pointer-events-none" />
+          </button>
         </div>
 
         <button className = "text-2xl text-white w-[500px] h-12 bg-themeGreen rounded-lg mt-10 hover:scale-105"type="submit">Submit</button>
