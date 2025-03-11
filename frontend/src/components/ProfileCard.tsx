ae03c4c484705fe60af9aa11d163b29b4f7ebd40
lain561 <mckissicethan@gmail.com>
2025-03-11 00:05:23 -0400

    lil name change

--- frontend/src/components/ProfileCard.tsx
+++ frontend/src/components/ProfileCard.tsx
@@ -18,7 +18,7 @@ export default function profileCard(props){
         <div className='flex flex-col jusitfy-center items-center h-[210px] w-[210px]'>
             <img src={ProfilePic} alt="profile Icon" className="w-32 h-32 rounded-full relative top-3.5"/>
             <div className= { infoClass }>
-                <p className='text-center text-lg font-bold'>Kevin</p>
+                <p className='text-center text-lg font-bold'>John Doe</p>
                 <p className='text-center'>Project Manager</p>
             </div>
         </div>
