70a804fb3e436e3d5b5af66a8800817357316d4d
lain561 <mckissicethan@gmail.com>
2025-03-19 03:32:00 -0400

    basic layout mockup (gotta fix this tmrw)

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -4,8 +4,17 @@ import Navbar from "../components/Navbar";
 export default function DashboardPage(){
 
     return (
-        <>
+        <main className="p-6">
             <Navbar />
-        </>
+            <section 
+            className="
+                flex flex-row items-center 
+                justify-center gap-12 
+                h-screen w-full">
+                <div className="bg-themeGray w-full h-[75%] rounded-md"></div>
+                <div className="bg-themeGray w-full h-[75%] rounded-md"></div>
+                <div className="bg-themeGray w-full h-[75%] rounded-md"></div>
+            </section>
+        </main>
     );
 }
