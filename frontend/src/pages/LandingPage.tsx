87d4dbca9c4bd19032f7fefd87f9a721565ad96c
Kevin Pereda <kptwdo15@gmail.com>
2025-03-24 21:16:51 -0400

    Added a bunch of lg breakpoints to landingpage because how tailwind works is by default its styles apply to mobile screens and lg breakpoint will apply styles only to screens with the resolution of a desktop for example. So now I have the responsivness for mobile devices and the desktop layout working. Just need to add md breakpoints next which will apply styles so that the page looks good on tablet screen sizes.

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -18,34 +18,35 @@ export default function LandingPage(){
     text-3xl 
     bg-themeGray 
     border-4 
-    border-themeDarkBeige 
-    w-44 h-16 rounded-lg
+    border-themeDarkBeige
+    w-36 h-12
+    lg:w-44 lg:h-16 rounded-lg
     cursor-pointer
     hover:scale-105
   `;
 
   return (
 
-    <div className = "relative flex justify-start items-start h-screen pt-24 gap-20 -translate-x-36"> {/* Outer div takes up whole screen so the inner div can be centered on screen */}
+    <div className = "relative flex flex-col justify-start items-center h-screen gap-4 lg:pt-24 lg:gap-20 lg:flex-row lg:-translate-x-36 lg:items-start"> {/* Outer div takes up whole screen so the inner div can be centered on screen */}
 
-          <div className = "relative flex flex-col items-center justify-center bg-themeGray w-4/6 h-5/6 rounded-lg flex-shrink-0 gap-10"> {/*This div is the gray card containing the closet picture and buttons under it */}
+          <div className = "order-2 relative flex flex-col items-center justify-center bg-themeGray rounded-lg flex-shrink-0 w-80 h-80 gap-4 pt- lg:gap-10 lg:w-4/6 lg:h-5/6 lg:pt-0 lg:order-1 "> {/*This div is the gray card containing the closet picture and buttons under it */}
 
-            <img src = {closetImage} alt = "Minimalist closet pic" className = "select-none pointer-events-none w-5/6 h-4/6 rounded-lg"></img> 
+            <img src = {closetImage} alt = "Minimalist closet pic" className = "select-none pointer-events-none w-64 h-56 rounded-lg lg:w-5/6 lg:h-4/6"></img> 
 
-            <div className = "flex flex-row justify-center items-center self-start gap-2 ml-16 w-fit"> {/*This div keeps all buttons in a flex row within the flex col that stacks the image and button above each other */}
-              <img src = {blackheart} alt = "heart symbol" className = "select-none pointer-events-none w-14 h-14"></img>
-              <img src = {chatbubble} alt = "chat bubble symbol" className = "select-none pointer-events-none w-14 h-14"></img>
-              <img src = {sendbutton} alt = "send button symbol" className = "select-none pointer-events-none w-14 h-14"></img>
-              <img src = {bookmark} alt = "bookmark symbol" className = "select-none pointer-events-none w-14 h-14 absolute right-16"></img>
+            <div className = "flex flex-row justify-center items-center self-start ml-8 w-fit gap-2 lg:ml-16"> {/*This div keeps all buttons in a flex row within the flex col that stacks the image and button above each other */}
+              <img src = {blackheart} alt = "heart symbol" className = "select-none pointer-events-none w-9 h-9 lg:w-14 lg:h-14"></img>
+              <img src = {chatbubble} alt = "chat bubble symbol" className = "select-none pointer-events-none  w-9 h-9 lg:w-14 lg:h-14"></img>
+              <img src = {sendbutton} alt = "send button symbol" className = "select-none pointer-events-none  w-9 h-9 lg:w-14 lg:h-14"></img>
+              <img src = {bookmark} alt = "bookmark symbol" className = "select-none pointer-events-none w-9 h-9 absolute right-7 lg:right-16 lg:w-14 lg:h-14"></img>
             </div>
 
           </div>  
 
-          <div className="flex flex-col flex-shrink-0 gap-16 relative"> {/*Div for dressmeup logo and slogan that will go under the logo*/}
+          <div className="flex flex-col flex-shrink-0 gap-16 relative lg:order-2"> {/*Div for dressmeup logo and slogan that will go under the logo*/}
 
-            <img src = {logo} alt = "DressMeUp Logo" className = "select-none pointer-events-none w-44 h-44 rounded-lg" />
+            <img src = {logo} alt = "DressMeUp Logo" className = "select-none pointer-events-none w-36 h-36 lg:w-44 lg:h-44 rounded-lg" />
 
-            <div className = "absolute top-[118%] w-[600px]"> {/*So I had to wrap the text in a div that was absolute to remove the text from page flow so they wouldnt push flex box with buttons next to it away. This div is relative to the flex col parent */}
+            <div className = "hidden lg:block lg:absolute lg:top-[118%] lg:w-[600px]"> {/*So I had to wrap the text in a div that was absolute to remove the text from page flow so they wouldnt push flex box with buttons next to it away. This div is relative to the flex col parent */}
               <p className  = "text-left text-7xl leading-normal"> {/*Align text left and since its div's width is fixed p tags wrap on their own so */}
                 Revamp your fashion.
                 Restyle your favorites. 
@@ -54,7 +55,8 @@ export default function LandingPage(){
             </div>
           </div>
 
-          <div className = "flex flex-col items-center justify-center gap-10 flex-grow-0"> {/*Flex col so that login and about us are stacked on top of each other */}
+
+          <div className = "flex items-center justify-center flex-grow-0 gap-8 lg:gap-10 lg:flex-col lg:order-2"> {/*Flex col so that login and about us are stacked on top of each other */}
 
             <button onClick = {() => navigate("/login")} className = {buttonClass}> Login  </button>
             <button onClick = {() => navigate("/about")} className = {buttonClass}> About Us </button>
@@ -62,9 +64,18 @@ export default function LandingPage(){
             {/*ABOVE ON CLICK NAVIGATES TO ABOUT US DOESNT EXIST CHANGE THAT ROUTE ONCE ETHAN MAKES ABOUT PAGE*/}
           </div>
 
-          <div className = "absolute -right-96 bottom-0"> {/*Plant picture is positioned abolsute (outside of page flow) relative to the screen div */}
-            <img src = {plantImage} alt = "plant pic" className = "select-none pointer-events-none w-96 h-80 " ></img>
+          <div className = "order-3 lg:hidden"> {/*So I had to wrap the text in a div that was absolute to remove the text from page flow so they wouldnt push flex box with buttons next to it away. This div is relative to the flex col parent */}
+              <p className  = "text-left text-3xl leading-normal whitespace-pre-line"> {/*Align text left and since its div's width is fixed p tags wrap on their own so */}
+                Revamp your fashion. <br />
+                Restyle your favorites. <br />
+                Rewear, reimagined. 
+              </p>
           </div>
+
+          <div className = "hidden lg:block absolute -right-96 bottom-0"> {/*Plant picture is positioned abolsute (outside of page flow) relative to the screen div */}
+            <img src = {plantImage} alt = "plant pic" className = "select-none pointer-events-none w-32 h-24 lg:w-96 lg:h-80 " ></img>
+          </div>
+
     </div>
   );
 }
