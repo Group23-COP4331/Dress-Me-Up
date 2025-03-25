90a7a27fca4958f82131b41b025a218eaadb9f7d
ethan mckissic <mckissicethan@gmail.com>
2025-03-25 14:03:45 -0400

    added calendar placeholder image

--- frontend/src/components/DashboardCard.tsx
+++ frontend/src/components/DashboardCard.tsx
@@ -4,9 +4,9 @@ export default function DashboardCard(props){
     return (
         <Link to="." className="bg-themeGray w-[650px] h-[700px] rounded-3xl flex flex-col items-center justify-center shadow-lg px-10 ">
             {/* Rembmer to adjust margins when I refactor this code*/}
-            <h1 className="text-5xl z-[2] text-white font-semibold mb-8 drop-shadow-lg">{props.title}</h1>
+            <h1 className="text-5xl z-[2] text-white font-bold mb-8 drop-shadow-lg">{props.title}</h1>
             <p className=" mb-10 z-[2] text-xl text-white text-wrap font-semibold sdrop-shadow-lg">{props.desc}</p>
-            <img src={props.pic} className='z-[1] relative object-cover rounded-3xl shadow-lg'></img>
+            <img src={props.pic} className='z-[1] object-cover rounded-3xl shadow-lg'></img>
         </Link>
     );
 }
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import DashboardCard from "../components/DashboardCard";
 import closetImage from "../assets/Dashboard/mycloset.jpg";
-import calendarImage from "../assets/Dashboard/mycloset.jpg";
+import calendarImage from "../assets/Dashboard/calendar.jpeg";
 
 export default function DashboardPage(){
     const previews = [
         {title: 'My Closet', desc: 'Your wardrobe, reimagined. Organize, favorite, and style your outfits effortlessly.', pic: closetImage},
-        {title: 'Outfit of the Day', desc:'Stay ahead of the forecast and seamlessly plan outfits for every occasion.', pic: closetImage}
+        {title: 'Outfit of the Day', desc:'Stay ahead of the forecast and seamlessly plan outfits for every occasion.', pic: calendarImage}
     ];
     // Removes default padding and width opon rendering
     useEffect(() => {
