ba01dbd5738006a9db9a023f4f841685864acd01
ethan mckissic <mckissicethan@gmail.com>
2025-03-25 13:59:48 -0400

    Update DashboardCard.tsx

--- frontend/src/components/DashboardCard.tsx
+++ frontend/src/components/DashboardCard.tsx
@@ -6,7 +6,7 @@ export default function DashboardCard(props){
             {/* Rembmer to adjust margins when I refactor this code*/}
             <h1 className="text-5xl z-[2] text-white font-semibold mb-8 drop-shadow-lg">{props.title}</h1>
             <p className=" mb-10 z-[2] text-xl text-white text-wrap font-semibold sdrop-shadow-lg">{props.desc}</p>
-            <img src={props.pic} className='z-[1] relative object-cover rounded-3xl'></img>
+            <img src={props.pic} className='z-[1] relative object-cover rounded-3xl shadow-lg'></img>
         </Link>
     );
 }
