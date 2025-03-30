932ba8df9f42de8d4029c51624fd73f1f422a682
ErnestoPerez2801 <147225351+ErnestoPerez2801@users.noreply.github.com>
2025-03-30 17:22:18 -0400

    Added MyCalendar Page

---
+++ frontend/src/pages/MyCalendarPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calendar from '../components/Calendar.tsx';
+import logo from "../assets/GreenLogo.png";
+import shrub from "../assets/MyCalendarImages/shrub.png";
+
+export default function MyCalendar() {
+  return (
+    
+    <div>
+      <h1>Monthly Calendar</h1>
+      <Calendar />
+    </div>
+  );
+}
