9b7d4fb88664d1fa803c9cd1aeb2fcc369a160f1
Daniel <danielperera0111@gmail.com>
2025-04-01 00:50:18 -0400

    Edited clothing item endpoints to properly get schemas

--- backend/api.js
+++ backend/api.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 const User = require('./models/user');
 const Card = require('./models/card');
 const Weather = require('./models/weather');
